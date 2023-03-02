61d95e4cc1c9217daa865846748b15478ea50d85
Samuel Raposo <samuel.raposo@icloud.com>
2023-03-02 01:07:24 -0300

    exerciciosJsAula11

--- alunos/samuel_raposo/capitulo_02/aula_11/paraCasa.js
+++ alunos/samuel_raposo/capitulo_02/aula_11/paraCasa.js
@@ -5,24 +5,52 @@
 // subtração -
 // multiplicação *
 // divisão /
+{
+    let num1 = 1000;
+    let num2 = 1500;
+    let resultado = num1 + num2;
+    console.log(resultado )
+  }
 
-
-    { 
-    var num1 = '1000';
-    var num2 = '2000';
-    var resultado= num1 + num2 
-    
-    }
-//---------------------------------------------------------------------------------------------------------------------------------------------//
 
 // EXERCÍCIO 02
 // Escrever um programa em JavaScript que utilize variáveis do tipo string para armazenar o nome, 
 // sobrenome e idade de uma pessoa e imprima uma mensagem no console 
+{
+    let nome1 = 'Samuel'
+    let nome2 = 'Antonio'
+    let nome3 = 'Martins'
+    let nome4 = 'Raposo'
+  
+      console.log(nome1+" "+nome2+" "+nome3+" "+nome4)
+  }
 
+  
 // EXERCÍCIO 03
 // Escrever um programa que utilize variáveis do tipo objeto para representar informações sobre um filme, 
 // como título, diretor, elenco e ano de lançamento. Imprimir essas informações no console.
+{
+    let biopedancia = {
+      idade: "27 anos",
+      peso: "80kg",
+      altura: "1.75",
+      imc: "28%",
+      }
+      console.log(biopedancia)
+    }
 
 // EXERCÍCIO 04
 // Criar um programa que utilize variáveis do tipo array para armazenar uma lista de 
 // nomes e imprimir essa lista no console.
+{
+
+    var gestores = ['Alan', 'Emerson', 'Junior','Rogerio', 'Lucas', 'Kisley', 'Angelica',
+                   'Margareth', 'Marcos', 'Eduardo Luis', 'Carlos']
+    
+  
+    console.log(gestores + "\n" +  "Os gestores da Região Metropolitana são:"); "\n"
+    console.log(gestores[0])
+    
+  }
+  
+    
