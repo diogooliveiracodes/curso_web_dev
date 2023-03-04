79ce2483ead93826f26fa12ad3809e7c22c804f7
maskked145 <henrriquenuunes@gmail.com>
2023-03-03 23:46:20 -0300

    Para casa aula11

---
+++ alunos/henrque_nunes/Capitulo01/Capitulo2/aula11/para_casa.js
@@ -0,0 +1,38 @@
+/* EXERCICIO 1 */
+var numero1 = 12;
+var numero2 = 14;
+var numero3 = 4;
+var numero4 = 6;
+var soma = numero1 + numero2
+var subtraçao = numero2 - numero3
+var multiplicaçao = numero3 * numero4
+var divisao = numero1 / numero3
+console.log(soma)
+console.log(subtraçao)
+console.log(multiplicaçao)
+console.log (divisao)
+
+/* EXERCICIO 2 */
+{
+    var nome = 'Henrique';
+    var sobrenome = 'Nunes';
+    var idade = '22';
+    var concatenaçao = nome + sobrenome + idade
+    console.log(concatenaçao)
+    console.log(nome)
+    console.log(sobrenome)
+    console.log(idade)
+  }
+//   EXERCICIO3
+var Filme = {
+    Titulo: 'Piratas da Informatica',
+    Diretor: 'Martyn Burke',
+    Elenco: ['Noah Wyle, Anthony Michael Hall, Joey Slotnick, John DiMaggio'],
+    AnoDeLançamento: 1999,
+}
+// EXERCICIO4
+var lista = ['Henrique', 'Ruan', 'Gustavo', 'Pedro']
+console.log(lista)
+console.log(lista[0])
+console.log(lista[3])
+  
