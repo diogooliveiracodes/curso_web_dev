a90c50a93c56e318fe62bacb88e58e394a28cea8
Samuel Raposo <samuel.raposo@icloud.com>
2023-03-04 01:27:37 -0300

    exerc_Aula12_Arrays

---
+++ alunos/samuel_raposo/capitulo_02/aula_12/paraCasa_01.js
@@ -0,0 +1,22 @@
+// 1. Crie uma lista com 5 ítens
+    var lista = [`Samuel`, `Sabrina`, `Helena`, `Denia`, `Antonio`, `Marielle`]
+    console.log (lista)
+
+// 2. Atribua novos valores a lista
+    lista[0] = `Samuel Raposo`, lista[1] = `Sabrina Mairink`
+
+// 3. Imprima a lista no console
+    console.log(lista)
+
+// 4. Imprima a quantidade de ítens da lista
+    console.log(lista.length)
+
+// 5. Imprima o índex do ítem de índice 3 da sua lista utilizando o valor atribuído a ele
+    lista[3] = lista[3] + " Junia"
+    console.log(lista[3])
+
+// 6. Crie uma nova lista e adicione a lista anterior como um ítem dela
+    var lista2 = [lista]
+
+// 7. Imprima a primeira lista através da segunda lista
+    console.log(lista2)
