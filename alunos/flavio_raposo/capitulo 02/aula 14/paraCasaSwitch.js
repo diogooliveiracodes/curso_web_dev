1e98a68cbd4a63baf8730f7a4009ba0952fb887e
FlavioRaposo7l <flavio.raposo.7l@gmail.com>
2023-03-09 22:28:51 -0300

    Parte Para Casa Aula 14

--- alunos/flavio_raposo/capitulo 02/aula 14/paraCasaSwitch.js
+++ alunos/flavio_raposo/capitulo 02/aula 14/paraCasaSwitch.js
@@ -30,3 +30,92 @@ switch (diaDaSemana) {
 
 //Para Casa 02
 
+var mes = 'Maio';
+
+switch(mes) {
+  case 'Janeiro':
+    console.log('O mês de '+ mes+ ' tem 31 dias.');
+    break;
+  case 'Fevereiro':
+    console.log('O mês de '+mes+ ' tem 28 ou 29 dias.');
+    break;
+  case 'Março':
+     console.log('O mês de '+ mes+ ' tem 31 dias.');
+    break
+  case 'Abril':
+     console.log('O mês de '+ mes+ ' tem 30 dias.');
+    break;
+  case 'Maio':
+     console.log('O mês de '+ mes+ ' tem 31 dias.');
+    break;
+  case 'Junho':
+     console.log('O mês de '+ mes+ ' tem 30 dias.');
+    break;  
+  case 'Julho':
+     console.log('O mês de '+ mes+ ' tem 31 dias.');
+    break;  
+  case 'Agosto':
+     console.log('O mês de '+ mes+ ' tem 31 dias.');
+    break;
+  case 'Setembro':
+     console.log('O mês de '+ mes+ ' tem 30 dias.');
+    break;
+  case 'Outubro':
+     console.log('O mês de '+ mes+ ' tem 31 dias.');
+    break;
+  case 'Novembro':
+     console.log('O mês de '+ mes+ ' tem 30 dias.');
+    break;
+  case 'Dezembro':
+     console.log('O mês de '+ mes+ ' tem 31 dias.');
+    break;  
+  default:
+    console.log('Mês inválido. Digite o mês com a inicial maiúscula.')
+}
+
+//Para Casa 03
+var mes = 'Maio';
+
+switch(mes) {
+  case 'Janeiro':
+    console.log('O mês de '+ mes+ ' pertence ao primeiro trimestre.');
+    break;
+  case 'Fevereiro':
+    console.log('O mês de '+mes+ ' pertence ao primeiro trimestre.');
+    break;
+  case 'Março':
+     console.log('O mês de '+ mes+ ' pertence ao primeiro trimestre.');
+    break
+  case 'Abril':
+     console.log('O mês de '+ mes+ ' pertence ao segundo trimestre.');
+    break;
+  case 'Maio':
+     console.log('O mês de '+ mes+ ' pertence ao segundo trimestre.');
+    break;
+  case 'Junho':
+     console.log('O mês de '+ mes+ ' pertence ao segundo trimestre.');
+    break;  
+  case 'Julho':
+     console.log('O mês de '+ mes+ ' pertence ao terceiro trimestre.');
+    break;  
+  case 'Agosto':
+     console.log('O mês de '+ mes+ ' pertence ao terceiro trimestre.');
+    break;
+  case 'Setembro':
+     console.log('O mês de '+ mes+ ' pertence ao terceiro trimestre.');
+    break;
+  case 'Outubro':
+     console.log('O mês de '+ mes+ ' pertence ao quarto trimestre.');
+    break;
+  case 'Novembro':
+     console.log('O mês de '+ mes+ ' pertence ao quarto trimestre.');
+    break;
+  case 'Dezembro':
+     console.log('O mês de '+ mes+ ' pertence ao quarto trimestre.');
+    break;  
+  default:
+    console.log('Mês inválido. Digite o mês com a inicial maiúscula.')
+}
+
+//Para Casa 04
+
