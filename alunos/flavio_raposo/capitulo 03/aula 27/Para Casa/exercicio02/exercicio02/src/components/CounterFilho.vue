f2d71edce8ebd90ce08b45490562de06ac18dd56
FlavioRaposo7l <flavio.raposo.7l@gmail.com>
2023-04-12 01:29:50 -0300

    Para Casa Aula 27

---
+++ alunos/flavio_raposo/capitulo 03/aula 27/Para Casa/exercicio02/exercicio02/src/components/CounterFilho.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Counter : {{ counterFilho }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'CounterFilho',
+    props: {
+        counterFilho: Number 
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ alunos/flavio_raposo/capitulo 03/aula 27/Para Casa/exercicio02/exercicio02/src/components/CounterParaCasa.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <CounterFilho v-bind:counterFilho = "counter"/>
+        <button @click="counter++">Adicionar 1</button>
+        <button @click="counter--">Diminuir 1</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CounterFilho from './CounterFilho.vue';
+
+export default defineComponent({
+    name: 'CounterParaCasa',
+    components: {
+        CounterFilho
+    },
+    data () {
+        return {
+            counter: 0
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
