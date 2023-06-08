9346302265888b5ffa9b5649bb68c3aca23ea5bf
Diogo Oliveira <diogo.o@icloud.com>
2023-06-07 21:20:34 -0300

    Aula 54: Projeto Prático - Autenticação

--- capitulo_05/projeto_pratico/resources/views/publicviews/home.blade.php
+++ capitulo_05/projeto_pratico/resources/views/publicviews/home.blade.php
@@ -22,7 +22,7 @@
                     @foreach($posts as $post)
                         <!-- Post preview-->
                         <div class="post-preview">
-                            <a href="post.html">
+                            <a href="{{route('post.show', $post)}}">
                                 <h2 class="post-title">{{$post->title}}</h2>
                                 <h3 class="post-subtitle">{{$post->content}}</h3>
                             </a>
