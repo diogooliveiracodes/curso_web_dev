a24b50ab3a30a6be89f762d52e3792bbc86ced36
Diogo Oliveira <diogo.o@icloud.com>
2023-07-21 11:12:21 -0300

    Aula 66

---
+++ capitulo_06/catalogo-de-produtos/app/Http/Controllers/ProductController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('products.index', [
+            'products' => Product::where('company_id', auth()->user()->company_id)->paginate(20)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+        Product::create($request->validated());
+
+        return redirect()->route('products.index')->with('success', 'Product created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return view('products.show', [
+            'Product' => Product::findOrFail($id)
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view('products.edit', [
+            'Product' => Product::findOrFail($id)
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $Product)
+    {
+        $Product->update($request->validated());
+
+        return redirect()
+            ->route('products.index')
+            ->with('success', 'Product updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Product::findOrFail($id)->delete();
+
+        return redirect()
+            ->route('products.index')
+            ->with('success', 'Product deleted successfully.');
+    }
+}
--- capitulo_06/catalogo-de-produtos/resources/views/categories/index.blade.php
+++ capitulo_06/catalogo-de-produtos/resources/views/categories/index.blade.php
@@ -1,18 +1,26 @@
 <x-app-layout>
 
-    <div class="flex justify-between bg-gray-800 w-full p-8">
-        <div class="">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Categories') }}
-            </h2>
-        </div>
-        <div class="">
-            <a href="{{route('categories.create')}}"
+    <div class="bg-gray-800 w-full p-8 px-4 py-8 sm:p-8">
+        <div class="max-w-7xl mx-auto px-0 sm:px-6 lg:px-8 flex justify-between">
+            <div>
+                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    {{ __('Categories') }}
+                </h2>
+            </div>
+            <div class="">
+                <a href="{{route('categories.create')}}"
                 class="dark:text-gray-200 leading-tight">New Category</a>
+            </div>
         </div>
     </div>
 
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div class="flex justify-center mt-4">
+        <div>
+            {{ $categories->links() }}
+        </div>
+    </div>
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-4 sm:mx-auto max-w-7xl">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
@@ -27,18 +35,21 @@
             <tbody>
                 @foreach($categories as $category)
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 overflow-clip">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-normal text-wrap dark:text-white">
+                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-normal text-wrap dark:text-white">
                         <a href="{{route('categories.show', $category)}}">
                             {{ $category->name }}
                         </a>
-                    </th>
-                    <td class="px-6 py-4 md:space-x-4">
-                        <a href="{{route('categories.edit', $category->id)}}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    </td>
+                    <td class="px-6 py-4">
+                        <a href="{{route('categories.edit', $category->id)}}"
+                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                            Edit
+                        </a>
                         <form action="{{route('categories.destroy', $category)}}" method="post"
                                 onsubmit="return confirm('Tem certeza que deseja excluir esta categoria?')">
                                 @csrf
                                 @method('DELETE')
-                            <button href="" class="font-medium text-red-600 dark:text-red-500 hover:underline">
+                            <button class="font-medium text-red-600 dark:text-red-500 hover:underline">
                                 Delete
                             </button>
                         </form>
